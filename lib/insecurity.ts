0393d5c78dde8b7af6c8780817a1dc46cf93c16e
Franke Telgenhof <franke.telgenhof@postcodeloterij.nl>
2025-12-03 12:12:04 +0100

    Fix vulnerabilities part III

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -7,7 +7,7 @@ import fs from 'fs'
 import crypto from 'crypto'
 import { type Request, type Response, type NextFunction } from 'express'
 import { type UserModel } from 'models/user'
-import expressJwt from 'express-jwt'
+import { expressjwt } from 'express-jwt'
 import jwt from 'jsonwebtoken'
 import jws from 'jws'
 import sanitizeHtmlLib from 'sanitize-html'
@@ -51,14 +51,17 @@ export const cutOffPoisonNullByte = (str: string) => {
   return str
 }
 
-export const isAuthorized = () => expressJwt(({ secret: publicKey }) as any)
-export const denyAll = () => expressJwt({ secret: '' + Math.random() } as any)
+export const isAuthorized = () => expressjwt({ secret: publicKey, algorithms: ['RS256'] })
+export const denyAll = () => expressjwt({ secret: '' + Math.random(), algorithms: ['RS256'] })
 export const authorize = (user = {}) => jwt.sign(user, privateKey, { expiresIn: '6h', algorithm: 'RS256' })
 export const verify = (token: string) => token ? (jws.verify as ((token: string, secret: string) => boolean))(token, publicKey) : false
-export const decode = (token: string) => { return jws.decode(token).payload }
+export const decode = (token: string) => {
+  const decoded = jws.decode(token)
+  return decoded ? decoded.payload : null
+}
 
 export const sanitizeHtml = (html: string) => sanitizeHtmlLib(html)
-export const sanitizeLegacy = (input = '') => input.replace(/<(?:\w+)\W+?[\w]/gi, '')
+export const sanitizeLegacy = (input = '') => input.replace(/<\w+\W+?[\w]/gi, '')
 export const sanitizeFilename = (filename: string) => sanitizeFilenameLib(filename)
 export const sanitizeSecure = (html: string): string | null => {
   const sanitized = sanitizeHtml(html)
@@ -194,7 +197,7 @@ export const appendUserId = () => {
 export const updateAuthenticatedUsers = () => (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.token || utils.jwtFrom(req)
   if (token) {
-    jwt.verify(token, publicKey, (err: Error | null, decoded: any) => {
+    jwt.verify(token, publicKey, { algorithms: ['RS256'] }, (err: Error | null, decoded: any) => {
       if (err === null) {
         if (authenticatedUsers.get(token) === undefined) {
           authenticatedUsers.put(token, decoded)
